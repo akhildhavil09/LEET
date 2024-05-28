7edf7265095db7ea691f522bb01bc72af607088b
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-05-28 14:09:35 +0530

    Time: 1181 ms (5.11%) | Memory: 30.6 MB (28.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,23 @@
 class Solution(object):
     def numIslands(self, grid):
-        m, n = len(grid), len(grid[0])
-        ans = 0
-        
-        def dfs(i, j):
-            grid[i][j] = "0"
-
-            if i - 1 >= 0 and grid[i - 1][j] == "1":
-                dfs(i - 1, j)
-            if i + 1 < m and grid[i + 1][j] == "1":
-                dfs(i + 1, j)
-            
-            if j + 1 < n and grid[i][j + 1] == "1":
-                dfs(i, j + 1)
-            if j - 1 >= 0 and grid[i][j - 1] == "1":
-                dfs(i, j - 1)
-            
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    dfs(i, j)
-                    ans += 1
-        return ans
+        visited=set()
+        def bfs(r,c):
+            q= collections.deque()
+            q.append((r,c))
+            visited.add((r,c))
+            while q:
+                row,col=q.popleft()
+                directions= [[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    nr,nc= row+dr, col+dc
+                    if (nr) in range(rows) and (nc) in range(cols) and grid[nr][nc]=="1" and (nr,nc) not in visited:
+                        q.append((nr,nc))
+                        visited.add((nr,nc))
+        island=0
+        rows,cols= len(grid), len(grid[0])
+        for r in range (rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and (r,c) not in visited:
+                    bfs(r,c)
+                    island+=1
+        return island
