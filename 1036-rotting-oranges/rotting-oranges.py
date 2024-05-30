bf21d19ccae991c69ec6148170c83f1d12ba8468
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-05-30 17:22:55 +0530

    Time: 20 ms (98.06%) | Memory: 11.5 MB (96.45%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,35 +1,27 @@
-from collections import deque
-
 class Solution(object):
     def orangesRotting(self, grid):
-        rows, cols = len(grid), len(grid[0])
-        q = deque()
-        fresh, time = 0, 0
-        
-        # Initialize the queue with all rotten oranges and count fresh oranges
+        rows,cols= len(grid), len(grid[0])
+        q=deque()
+        time,fresh=0,0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh += 1
-                if grid[r][c] == 2:
-                    q.append((r, c))
-        
-        def convert(r, c):
-            if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1:
-                grid[r][c] = 2
-                q.append((r, c))
+                if grid[r][c]==1:
+                    fresh+=1
+                if grid[r][c]==2:
+                    q.append((r,c))
+        def rotten(r,c):
+            if (0<=r<rows and 0<=c<cols and grid[r][c]==1):
+                grid[r][c]=2
+                q.append((r,c))
                 return 1
             return 0
-        
-        # BFS process
-        while q and fresh > 0:
-            # Process all oranges in the current level of BFS
+        while q and fresh>0:
             for _ in range(len(q)):
-                row, col = q.popleft()
-                fresh -= convert(row + 1, col)
-                fresh -= convert(row - 1, col)
-                fresh -= convert(row, col + 1)
-                fresh -= convert(row, col - 1)
-            time += 1
-        
-        return time if fresh == 0 else -1
+                ro,co= q.popleft()
+                fresh -= rotten(ro+1,co)
+                fresh -= rotten(ro-1,co)
+                fresh -= rotten(ro,co+1)
+                fresh -= rotten(ro,co-1)
+            time+=1
+        return time if fresh==0 else -1
+                        
