b9d7213ba987c6c9392481efb75e5dd2ebb31ed5
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-07-16 11:01:48 -0400

    Time: 11 ms (87.43%) | Memory: 11.5 MB (88.52%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def reverse(self, x):
-        rev_x= 0
-        sign=-1 if x<0 else 1
-        x=abs(x)
-
+        rev=0
         maxint= 2**31 -1
-        while x>0:
-            digit= x%10
+        sign= -1 if x<0 else 1
+        x= abs(x)
 
-            if rev_x > maxint //10 or (rev_x== maxint //10 and digit>7):
+        while x:
+            digit=x%10
+            
+            if rev>maxint //10 or (rev==maxint //10  and digit >7):
                 return 0
-            rev_x= rev_x * 10 + digit
+            rev= (rev*10) + digit
+            
             x//=10
-        return rev_x*sign
+        return rev*sign
