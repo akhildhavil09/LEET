7378edaa0dde09c4e56027415b9ece9dd48d4dff
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-07-25 21:04:40 -0400

    Time: 34 ms (48.65%) | Memory: 19.2 MB (78.60%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -7,17 +7,17 @@
 class Solution(object):
     def kthSmallest(self, root, k):
         self.count=0
-        self.result= None
+        self.result=0
         self.inorder(root,k)
         return self.result
-    def inorder(self,node,k):
-        if not node or self.count>k: return 
 
+    def inorder(self,node,k):
+        if not node or self.count>=k:
+            return
         self.inorder(node.left,k)
         self.count+=1
-
         if self.count==k:
-            self.result= node.val
-            return
-        
+            self.result=node.val
         self.inorder(node.right,k)
+        
+
