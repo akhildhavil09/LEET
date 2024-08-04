c0995bdab00679484d16d8b1f2ce07db158b7221
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-04 11:52:10 -0400

    Time: 485 ms (54.78%) | Memory: 30.8 MB (47.10%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,21 +1,23 @@
 class Solution(object):
     def partition(self, s):
-        def is_palindrome(sub):
-            return sub == sub[::-1]
-
-        def backtrack(start, path):
-            if start == len(s):
-                partitions.append(path[:])
+        res=[]
+        path=[]
+        def palinhelper(index):
+            if index==len(s):
+                res.append(path[:])
                 return
-
-            for end in range(start, len(s)):
-                substring = s[start:end + 1]
-                if is_palindrome(substring):
-                    path.append(substring)
-                    backtrack(end + 1, path)
+            for i in range(index,len(s)):
+                if ispalin(s,index,i):
+                    path.append(s[index:i+1])
+                    palinhelper(i+1)
                     path.pop()
-
-        partitions = []
-        backtrack(0, [])
-        return partitions
-
+        def ispalin(s,start,end):
+            while start<= end:
+                if s[start]!=s[end]:
+                    return False
+                start+=1
+                end-=1
+            return True
+        palinhelper(0)
+        return res
+        
