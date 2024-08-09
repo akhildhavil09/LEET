fcd885a6e288c483c3a649b0bc6fea93a6a5420a
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-09 09:03:19 -0400

    Time: 24 ms (91.44%) | Memory: 11.8 MB (9.47%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,27 +1,25 @@
 class Solution(object):
     def orangesRotting(self, grid):
-        rows,cols= len(grid), len(grid[0])
         q=deque()
+        directions=[[1,0],[-1,0],[0,1],[0,-1]]
         time,fresh=0,0
+        rows,cols=len(grid),len(grid[0])
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]==1:
-                    fresh+=1
                 if grid[r][c]==2:
                     q.append((r,c))
-        def rotten(r,c):
-            if (0<=r<rows and 0<=c<cols and grid[r][c]==1):
-                grid[r][c]=2
-                q.append((r,c))
-                return 1
-            return 0
+                elif grid[r][c]==1:
+                    fresh+=1
+        
         while q and fresh>0:
-            for _ in range(len(q)):
-                ro,co= q.popleft()
-                fresh -= rotten(ro+1,co)
-                fresh -= rotten(ro-1,co)
-                fresh -= rotten(ro,co+1)
-                fresh -= rotten(ro,co-1)
             time+=1
-        return time if fresh==0 else -1
-                        
+            for _ in range(len(q)):
+                r,c=q.popleft()
+                for dr,dc in directions:
+                    nr,nc= r+dr, c+dc
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
+                        grid[nr][nc]=2
+                        q.append((nr,nc))
+                        fresh-=1
+        return -1 if fresh>0 else time
