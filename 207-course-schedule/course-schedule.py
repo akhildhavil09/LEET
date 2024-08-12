ec72b975d9f9881a072f27e1e5e1e70e6c85320b
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-12 12:40:20 -0400

    Time: 69 ms (44.15%) | Memory: 14.8 MB (17.36%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,23 +1,26 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        prehash={i:[] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            prehash[crs].append(pre)
-        
-        visit=set()
-        def dfs(crs):
-            if crs in visit:return False
-            if prehash[crs]==[]: return True
-            visit.add(crs)
-            for pre in prehash[crs]:
-                if not dfs(pre):
-                    return False
-            visit.remove(crs)
-            prehash[crs]=[]
-            return True
-        for c in range(numCourses):
-            if not dfs(c):
+        graph={i:[] for i in range(numCourses)}
+
+        for crs1,crs2 in prerequisites:
+            graph[crs1].append(crs2)
+
+        visiting,visited=set(), set()
+
+        def hasCycle(crs):
+            if crs in visiting:
+                return True # has cycle
+            if crs in visited:
+                return False # no cycle found for this crs
+
+            visiting.add(crs)
+            for nei in graph[crs]:
+                if hasCycle(nei):
+                    return True
+            visiting.remove(crs)
+            visited.add(crs)
+            return False
+        for crs in range(numCourses):
+            if hasCycle(crs):
                 return False
         return True
-    
-        
