0e44da4ad93a842e60fe956aa441381fe4eaccce
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-17 22:40:46 -0400

    Time: 30 ms (63.36%) | Memory: 11.5 MB (73.81%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,7 +3,7 @@ class Solution(object):
         q=deque()
         directions=[[1,0],[-1,0],[0,1],[0,-1]]
         time,fresh=0,0
-        rows,cols=len(grid),len(grid[0])
+        rows,cols= len(grid), len(grid[0])
 
         for r in range(rows):
             for c in range(cols):
@@ -11,15 +11,16 @@ class Solution(object):
                     q.append((r,c))
                 elif grid[r][c]==1:
                     fresh+=1
-        
+            
         while q and fresh>0:
             time+=1
             for _ in range(len(q)):
-                r,c=q.popleft()
-                for dr,dc in directions:
-                    nr,nc= r+dr, c+dc
+                r,c= q.popleft()
+                for dr, dc in directions:
+                    nr,nc= dr+r, dc+c
                     if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
                         grid[nr][nc]=2
                         q.append((nr,nc))
                         fresh-=1
-        return -1 if fresh>0 else time
+        return -1  if fresh>0 else time
+        
