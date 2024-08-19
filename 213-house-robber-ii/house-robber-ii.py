646e6774db62d3039b9bfebbe8390fa45879d526
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-19 09:03:58 -0400

    Time: 24 ms (8.27%) | Memory: 11.5 MB (93.17%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def rob(self, nums):
         return max(nums[0],self.helper(nums[1:]),self.helper(nums[:-1]))
+
     def helper(self,nums):
         rob1,rob2=0,0
         for i in nums:
-            current=max(rob1+i, rob2)
+            current=max(rob1+i,rob2)
             rob1=rob2
             rob2=current
         return rob2
+        
