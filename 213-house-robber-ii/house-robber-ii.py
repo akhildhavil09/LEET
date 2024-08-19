45ea7e2bf30b1f8fcaf1c36c367b6c6fc5746281
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-19 09:04:39 -0400

    Time: 17 ms (43.53%) | Memory: 11.6 MB (34.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,12 +1,31 @@
-class Solution(object):
-    def rob(self, nums):
-        return max(nums[0],self.helper(nums[1:]),self.helper(nums[:-1]))
+class Solution:
+    def robLinear(self, nums):
+        n=len(nums)
+        if n==0:
+            return 0
+        if n==1:
+            return nums[0]
 
-    def helper(self,nums):
-        rob1,rob2=0,0
-        for i in nums:
-            current=max(rob1+i,rob2)
-            rob1=rob2
-            rob2=current
-        return rob2
+        dp=[0]*n
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+
+        for i in range(2,n):
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        
+        return dp[-1]
+
+    def rob(self,nums):
+        n=len(nums)
+        if n==0: return 0
+        if n==1: return nums[0]
+
+        #case1: Include first house, resulting cant include last house
+        case1= self.robLinear(nums[:-1])
+
+        #case2: Start from 2nd house, end at last house
+        case2= self.robLinear(nums[1:])
+
+        return max(case1,case2)
+    
         
