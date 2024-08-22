27c45fb17d6b5b0f2c4915464533b839e1033112
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-22 10:46:58 -0400

    Time: 549 ms (24.53%) | Memory: 24.7 MB (15.22%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        if not nums:return 0
-        max_current=max_global=nums[0]
+        if len(nums)==0: return 0
+        max_cur=max_global= nums[0]
+
         for num in nums[1:]:
-            max_current= max(num, max_current+num)
-            max_global=max(max_global, max_current)
-        
-        return max_global
+            max_cur=max(num,max_cur+num)
+            
+            max_global= max(max_global, max_cur)
+
+        return max_global        
