0854c41b30598f3cb0bbd2cff96fbcd5060dae87
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-08-30 16:53:49 -0400

    Time: 70 ms (13.43%) | Memory: 16.6 MB (43.62%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,17 +1,20 @@
-class Solution(object):
-    def combinationSum(self, nums, target):
-        res=[]
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         sub=[]
+        res=[]
 
         def dfs(index):
             if sum(sub)==target:
                 res.append(sub[:])
                 return
+            
             if sum(sub)>target or index>=len(nums):
                 return
+
             sub.append(nums[index])
             dfs(index)
             sub.pop()
             dfs(index+1)
+
         dfs(0)
         return res
