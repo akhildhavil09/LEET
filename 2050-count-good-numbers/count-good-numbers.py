8463f68f59874e7d5aa5b91e6f7d856f26829d62
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-10-18 10:02:11 -0400

    Time: 0 ms (100.00%) | Memory: 16.7 MB (18.51%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,11 +1,10 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        MOD=10**9 +7
+        MOD= 10**9 +7
 
-        #count of even and odd indices
-        even_count=(n+1)//2
-        odd_count=n//2
+        even= (n+1)//2
+        odd= n//2
 
-        total_good_string= (pow(5,even_count,MOD)* pow(4,odd_count,MOD)) %MOD
+        res= (pow(5,even,MOD)* pow(4,odd,MOD))% MOD
 
-        return total_good_string
+        return res
