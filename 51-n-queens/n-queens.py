2240c218ff9949ec4694ead4f2c3ad10647e6606
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-10-25 11:14:50 -0400

    Time: 44 ms (82.69%) | Memory: 17 MB (85.85%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,8 +1,8 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def isSafe(r,c,board,n):
-            dupli_r=r
-            dupli_c=c
+            dupli_r= r
+            dupli_c = c
 
             while r>=0 and c>=0:
                 if board[r][c]=="Q":
@@ -17,7 +17,7 @@ class Solution:
                 if board[r][c]=="Q":
                     return False
                 c-=1
-            
+
             c=dupli_c
             r=dupli_r
 
@@ -29,6 +29,7 @@ class Solution:
             
             return True
 
+
         def sol(col,board,ans,n):
             if col==n:
                 ans.append(list(board))
@@ -37,9 +38,9 @@ class Solution:
                 if isSafe(row,col,board,n):
                     board[row]=board[row][:col]+"Q"+board[row][col+1:]
                     sol(col+1,board,ans,n)
-                    board[row]=board[row][:col]+"."+ board[row][col+1:]
+                    board[row]=board[row][:col]+"."+board[row][col+1:]
 
         ans=[]
-        board=["."*n for _ in range(n)]
+        board=['.'*n for _ in range(n)]
         sol(0,board,ans,n)
         return ans
