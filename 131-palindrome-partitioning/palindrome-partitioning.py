1842ecf46f014ff2acb647c6394f186b88f029fa
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-10-31 08:11:05 -0400

    Time: 47 ms (67.94%) | Memory: 34 MB (72.15%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,26 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        n=len(s)
         res=[]
         path=[]
+        def ispalin(s,start,end):
+            while start<=end:
+                if s[start]!=s[end]:
+                    return False
+                start+=1
+                end-=1
+            return True
 
-        dp=[[False]*n for _ in range(n)]
-
-        for end in range(n):
-            for start in range(end+1):
-                if s[start]==s[end] and (end-start <=2 or dp[start+1][end-1]):
-                    dp[start][end]=True
-            
         def helper(index):
-            if index==n:
+            if index==len(s):
                 res.append(path[:])
-                return
-            
-            for i in range(index,n):
-                if dp[index][i]:
+                return 
+            for i in range(index, len(s)):
+                if ispalin(s,index,i):
                     path.append(s[index:i+1])
                     helper(i+1)
                     path.pop()
-
         helper(0)
         return res
