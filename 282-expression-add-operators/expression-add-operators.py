0649a3a287e95ffccbaac7dfe88fa00c3f934c34
akhildhavil09 <70201601+akhildhavil09@users.noreply.github.com>
2024-11-03 09:26:47 -0500

    Time: 460 ms (62.64%) | Memory: 16.9 MB (97.81%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.py
+++ 282-expression-add-operators/expression-add-operators.py
@@ -1,27 +1,26 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
-        def backtrack( index, prev_num, cur_res, expression):
+        def backtrack(index, prev_num, running_res, exp):
             if index==len(num):
-                if cur_res==target:
-                    result.append(expression)
+                if running_res==target:
+                    res.append(exp)
                 return
-
-            for i in range(index, len(num)):
+            
+            for i in range(index,len(num)):
                 if i!=index and num[index]=='0':
                     break
-                
                 cur_num= int(num[index:i+1])
 
                 if index==0:
                     backtrack(i+1, cur_num, cur_num, str(cur_num))
                 else:
-                    #addition
-                    backtrack(i+1, cur_num, cur_res+cur_num, expression+ '+' +str(cur_num))
-                    #subtraction
-                    backtrack(i+1, -cur_num, cur_res-cur_num, expression+ '-' +str(cur_num))
-                    #multiplication
-                    backtrack(i+1, prev_num*cur_num, cur_res-prev_num +(prev_num*cur_num), expression+ '*' + str(cur_num))
-        result=[]
-        if not num: return result
+                    backtrack(i+1,cur_num, running_res+cur_num, exp+'+'+str(cur_num))
+
+                    backtrack(i+1, -cur_num, running_res-cur_num, exp+'-'+str(cur_num))
+
+                    backtrack(i+1,prev_num*cur_num, running_res- prev_num +(prev_num*cur_num), exp+'*'+str(cur_num))
+        res=[]
+        if not num:
+            return res
         backtrack(0,0,0,"")
-        return result
+        return res
